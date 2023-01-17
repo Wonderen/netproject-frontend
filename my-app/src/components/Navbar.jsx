251b8d4d34e3c8a8e6b77a97561f988478ff8985
Negaar <ngr.rzvnfr@gmail.com>
2023-01-17 19:06:36 +0330

    Fix Navbar

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="Nav">
+      <div className="Nav__container">
+        <Link to="/" className="Nav__brand">
+          <img src="../logo.svg" className="Nav__logo" />
+        </Link>
+
+        <div className="Nav__right">
+          <ul className="Nav__item-wrapper">
+            <li className="Nav__item">
+              <Link className="Nav__link" to="/login">Login</Link>
+            </li>
+            <li className="Nav__item">
+              <Link className="Nav__link" to="/signup">Sign up</Link>
+            </li>
+            <li className="Nav__item">
+              <Link className="Nav__link" to="/path3">Link 3</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
